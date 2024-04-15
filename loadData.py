dad3749e376a8ee3206580699079843f01b08579
JR-espacial <jorge_alan_2001@hotmail.com>
2024-04-14 19:53:48 -0600

    feat: first model

--- loadData.py
+++ loadData.py
@@ -1,6 +1,14 @@
+import time
 import numpy as np
 import os
 import cv2
+# TensorFlow and tf.keras
+import tensorflow as tf
+import torch
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
 
 def load_data(data_dir):
     images = []
@@ -30,7 +38,11 @@ def load_data(data_dir):
         labels.append(label)  # Append the not empty label to the labels list
         images.append(image)  # Append the loaded image to the images list
     
-    return np.array(images), np.array(labels, dtype=object)
+    # Convert lists to TensorFlow Tensors
+    images_tensor = tf.convert_to_tensor(np.array(images))
+    labels_tensor = tf.convert_to_tensor(np.array(labels))
+    
+    return images_tensor, labels_tensor
 
 # Directory of training data
 train_data_dir = "train"
@@ -39,6 +51,7 @@ test_data_dir = "test"
 # Load training data
 train_images, train_labels = load_data(train_data_dir)
 test_images, test_labels = load_data(test_data_dir)
+
 
 # Print the shape of the loaded data
 print("Number of training images:", train_images.shape)
@@ -49,20 +62,57 @@ print("Number of testing labels:", test_labels.shape)
 print("train_labels", train_labels)
 
 
-# notEmptytrain = 0
-# notEmptytest = 0
-# for label in train_labels:
-#     if len(label) > 0:
-#         # print( label[0]['class'])
-#         notEmptytrain  += 1
-#     else:
-#         print("empty", label)
-# for label in test_labels:
-#     if len(label) > 0:
-#         # print( label[0]['class'])
-#         notEmptytest  += 1
+class_names = ['Bacterial Spot', 'Early Blight', 'Healthy', 'Late Blight', 'Leaf Mold', 'Target Spot', 'Black Spot']
 
 
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+plt.show()
 
-# print(notEmptytrain)
-# print(notEmptytest )
+
+# Add a color dimension to the images
+train_images = tf.reshape(train_images, [train_images.shape[0], 640, 640, 3])
+test_images = tf.reshape(test_images, [test_images.shape[0], 640, 640, 3])
+
+# Normalize pixel values to be between 0 and 1
+train_images = tf.cast(train_images, tf.float32) / 255.0
+test_images = tf.cast(test_images, tf.float32) / 255.0
+
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+plt.show()
+
+time.sleep(2)
+
+
+# train model
+
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
+
+model = Sequential([
+    Flatten(input_shape=(640,640,3)),
+    Dense(128, activation='relu'),
+    Dense(7, activation='softmax')
+])
+
+"""
+    compile the model using:
+    for optimization use adam optimizer  'adam'
+    for loss use sparse categorcil crossentropy with logits use the object from tensorflow
+    tf.keras.losses.SparseCategoricalCrossentropy pass logits as true
+    for metrics use accuracy
+"""
+model.compile(optimizer= 'adam',loss= 'sparse_categorical_crossentropy'   ,metrics = ['accuracy'] )
+
+
+model.fit(train_images, train_labels, batch_size=32, epochs=8)
