6118dcf952e57ce39b686b2103e89596bab97dc4
JR-espacial <jorge_alan_2001@hotmail.com>
2024-04-22 01:49:46 -0600

    feat: reshape data

--- kernel.py
+++ kernel.py
@@ -8,6 +8,40 @@ from loadData import preprocess_images
 from model import train_model
 from tests import test_model
 
+def rebalance_datasets(train_images, train_labels, test_images, test_labels, val_images, val_labels, out_percentages):
+    
+    #Join all images and labels
+    all_images = np.concatenate((train_images, test_images, val_images))
+    all_labels = np.concatenate((train_labels, test_labels, val_labels))
+
+    #redestructure the data according to the out_percentages
+    new_train_images = []
+    new_train_labels = []
+    new_test_images = []
+    new_test_labels = []
+    new_val_images = []
+    new_val_labels = []
+
+    #out_percentages ex. = [0.5, 0.2, 0.3]
+    for i, percentage in  enumerate(out_percentages):
+        #train data and labels
+        if  i == 0:
+            train_limit = int(len(all_images)*percentage)
+            new_train_images = all_images[:train_limit]
+            new_train_labels = all_labels[:train_limit]
+        #val data and labels
+        elif i == 1:
+            val_limit = train_limit + int(len(all_images)*percentage)
+            new_val_images = all_images[train_limit:val_limit]
+            new_val_labels = all_labels[train_limit:val_limit]
+        #test data and labels
+        else:
+            new_test_images = all_images[val_limit:]
+            new_test_labels = all_labels[val_limit:]
+            
+    return new_train_images, new_train_labels, new_test_images, new_test_labels, new_val_images, new_val_labels
+    
+
 def show_dataset(images,labes,class_names,title):
     plt.figure(figsize=(10,10))
     for i in range(25):
@@ -32,16 +66,26 @@ def execute(function_name, function, *args):
 # Directory of training data
 train_data_dir = "train"
 test_data_dir = "test"
+val_data_dir = "valid"
 
 # Load training data
 print("Loading data...")
 train_images, train_labels = load_data(train_data_dir)
 test_images, test_labels = load_data(test_data_dir)
+val_images, val_labels = load_data(val_data_dir)
+
+#Rebalance the data
+#percentage of the data to be used in the training, validation and testing
+# [train, val, test]
+new_data_shape = [0.6, 0.1, 0.3]
+train_images, train_labels, test_images, test_labels, val_images, val_labels = rebalance_datasets(train_images, train_labels, test_images, test_labels, val_images, val_labels, new_data_shape)
 
 
 #Shape of the data
 print("Shape of training images:", train_images.shape)
 print("Shape of training labels:", train_labels.shape)
+print("Shape of validation images:", val_images.shape)
+print("Shape of validation labels:", val_labels.shape)
 print("Shape of testing images:", test_images.shape)
 print("Shape of testing labels:", test_labels.shape)
 
@@ -57,6 +101,7 @@ class_names = [ 'Bacterial Spot', 'Early Blight',
 
 p_train_i = preprocess_images(train_images,128,128)
 p_test_i = preprocess_images(test_images,128,128)
+p_val_i = preprocess_images(val_images,128,128)
 
 
 
@@ -69,9 +114,10 @@ execute("show proceseed data",show_dataset, p_train_i,train_labels,class_names,"
 
 # prepare data to train
 images_tensor = tf.convert_to_tensor(np.array(p_train_i))
+val_images = tf.convert_to_tensor(np.array(p_val_i))
 
 #Train the model
-execute("train the model",train_model,images_tensor,train_labels)
+execute("train the model",train_model,images_tensor,train_labels,val_images,val_labels)
 
 
 
--- model.py
+++ model.py
@@ -4,16 +4,31 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential # type: ignore
 from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout # type: ignore
 
+
+
+
+
 # model definition
 def create_model():
     
+    #model v3
+    # model = Sequential([
+    #   Conv2D(32, (3, 3), activation='relu', input_shape=(128, 128, 3)),
+    #   MaxPooling2D((2, 2)),
+    #   Flatten(input_shape=(128,128,3)),
+    #   Dense(128, activation='relu'),
+    #   Dense(7, activation='softmax')
+    # ])
+    
+    #Model 4
+
     model = Sequential([
         Conv2D(100, (3, 3), activation='relu', input_shape=(128, 128, 3)),
         MaxPooling2D((2, 2)),
-        Dropout(0.25),
+        Dropout(0.20),
         Conv2D(64, (3, 3), activation='relu'),
         MaxPooling2D((2, 2)),
-        Dropout(0.25),
+        Dropout(0.20),
         Flatten(input_shape=(128,128,3)),
         Dense(128, activation='relu'),
         Dense(7, activation='softmax')
@@ -21,10 +36,10 @@ def create_model():
 
     return model
 
-def train_model(images_tensor, train_labels):
+def train_model(images_tensor, train_labels, val_images, val_labels):
     model = create_model()
     model.compile(optimizer= 'adam',loss= 'sparse_categorical_crossentropy',metrics = ['accuracy'] )
-    model.fit(images_tensor, train_labels, batch_size=32, epochs=10)
+    model.fit(images_tensor, train_labels, batch_size=32, epochs=10, validation_data=(val_images, val_labels))
     model.summary()
     model.save('jorge.h5')
     
