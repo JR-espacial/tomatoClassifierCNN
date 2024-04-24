56de56ff8a3f609480cea4a00c13d872a450bb2e
JR-espacial <jorge_alan_2001@hotmail.com>
2024-04-24 12:22:32 -0600

    feat : add label distribution

--- kernel.py
+++ kernel.py
@@ -8,7 +8,15 @@ from loadData import preprocess_images
 from model import train_model
 from tests import test_model
 
-def rebalance_datasets(train_images, train_labels, test_images, test_labels, val_images, val_labels, out_percentages):
+
+def label_distribution(labels, class_names):
+    #print how many labels of each class
+    for i in range(7):
+        print("Number of labels of class", i, class_names[i] , ":", np.sum(labels == i))
+
+        
+
+def rebalance_datasets(train_images, train_labels, test_images, test_labels, val_images, val_labels, out_percentages, classes):
     
     #Join all images and labels
     all_images = np.concatenate((train_images, test_images, val_images))
@@ -17,7 +25,7 @@ def rebalance_datasets(train_images, train_labels, test_images, test_labels, val
 
     #print how many labels of each class
     for i in range(7):
-        print("Number of labels of class", i, ":", np.sum(all_labels == i))
+        print("Number of labels of class", i, class_names[i] , ":", np.sum(all_labels == i))
 
     #redestructure the data according to the out_percentages
     new_train_images = []
@@ -79,12 +87,27 @@ train_images, train_labels = load_data(train_data_dir)
 test_images, test_labels = load_data(test_data_dir)
 val_images, val_labels = load_data(val_data_dir)
 
+# Define class names (7)
+class_names = [ 'Bacterial Spot', 'Early Blight', 
+                'Healthy', 'Late Blight', 'Leaf Mold', 
+                'Target Spot', 'Black Spot']
+
 #Rebalance the data
 #percentage of the data to be used in the training, validation and testing
 # [train, val, test]
-new_data_shape = [0.6, 0.1, 0.3]
-train_images, train_labels, test_images, test_labels, val_images, val_labels = rebalance_datasets(train_images, train_labels, test_images, test_labels, val_images, val_labels, new_data_shape)
+# new_data_shape = [0.6, 0.1, 0.3]
+# train_images, train_labels, test_images, test_labels, val_images, val_labels = rebalance_datasets(train_images, train_labels, test_images, test_labels, val_images, val_labels, new_data_shape, class_names)
 
+#Show the distribution of the labels
+print ("Training data distribution")
+label_distribution(train_labels,class_names)
+print("---------------------------------")
+print ("Validation data distribution")
+label_distribution(val_labels,class_names)
+print("---------------------------------")
+print ("Testing data distribution")
+label_distribution(test_labels,class_names)
+print("---------------------------------")
 
 #Shape of the data
 print("Shape of training images:", train_images.shape)
@@ -96,12 +119,6 @@ print("Shape of testing labels:", test_labels.shape)
 
 print("---------------------------------")
 
-
-
-# Define class names (7)
-class_names = [ 'Bacterial Spot', 'Early Blight', 
-                'Healthy', 'Late Blight', 'Leaf Mold', 
-                'Target Spot', 'Black Spot']
 
 
 p_train_i = preprocess_images(train_images,128,128)
